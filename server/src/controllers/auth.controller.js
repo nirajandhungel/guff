18413d1f78f53580718cc1edb77bff77007b434c
nirajandhungel <subashdhungel555@gmail.com>
2025-08-19 23:46:23 +0545

    Created auth controller and export router from auth.route

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = ()=> {
+
+}
+export const login = ()=> {
+
+}
+export const logout = ()=> {
+
+}
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-const router = express.Router()
-router.post("/signup",signUpRoute)
-router.post("/login",signUpRoute)
-router.post("/logout",signUpRoute)
-export default function signUpRoute(){
+import { signup ,login, logout} from "../controllers/auth.controller.js";
+const  router = express.Router()
+router.post("/signup",signup)
+router.post("/login",login)
+router.post("/logout",logout)
 
-}
+export default router;
