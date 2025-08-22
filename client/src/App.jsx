98c8fdc7769d7143518a4c45ff76bbb457964763
nirajandhungel <subashdhungel555@gmail.com>
2025-08-23 01:02:56 +0545

    feat(router): wrap App in BrowserRouter and add Home, Login, Signup, Settings, Profile routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Profile from './pages/Profile';
 const App = () => {
   return (
     <div className='text-red-600'>
       hello
+      <Navbar/>
+      <Routes>
+        <Route path ="/" element={<Home/>}></Route>
+        <Route path ="/signup" element={<SignUp/>}></Route>
+        <Route path ="/login" element={<Login/>}></Route>
+        <Route path ="/settings" element={<Settings/>}></Route>
+        <Route path ="/profile" element={<Profile/>}></Route>
+
+      </Routes>
 
       
       
