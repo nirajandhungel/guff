3d1ef46b872cbfb1ea86190d29ab93e809c1d9a3
nirajandhungel <subashdhungel555@gmail.com>
2025-08-25 00:32:46 +0545

    feat(action): create sign up action to server

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axiosInstance";
 import { API_PATHS } from "../lib/apiPaths";
+import { toast } from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp:false,
@@ -21,5 +22,32 @@ export const useAuthStore = create((set) => ({
     }finally{
       set({isCheckingAuth:false})
     }
+  },
+
+  signUp: async(data)=>{
+    set({isSigningUp:true});
+    try {
+      const res = await axiosInstance.post(API_PATHS.AUTH.SIGN_UP, data);
+      if(!res.data){
+        throw new Error("Sign up failed");
+        
+      }
+        
+
+      // set the auth user data in the store
+     set({ authUser: res.data});
+
+
+      //success message
+      toast.success("Sign up successful")
+
+    } catch (error) {
+      console.log("Error in signUp", error);
+      toast.error(error.response?.data?.message || "Sign up failed.")
+      
+    }finally{
+      set({isSigningUp:false});
+    }
+
   }
 }));
