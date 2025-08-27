42e61919031ce83cf8432b370d231e1f96b6b043
nirajandhungel <subashdhungel555@gmail.com>
2025-08-27 14:41:01 +0545

    refactor(dotenv): use simple dotenv config

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
-import { configDotenv } from "dotenv";
-configDotenv();
-
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
