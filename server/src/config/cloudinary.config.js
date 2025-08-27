c5e12ba98551e962133bfc8bf06d94eedaff1ef2
nirajandhungel <subashdhungel555@gmail.com>
2025-08-27 14:37:23 +0545

    fix(missing secret): call dotenv again on cloudinary config

--- server/src/config/cloudinary.config.js
+++ server/src/config/cloudinary.config.js
@@ -1,7 +1,14 @@
-import {v2 as cloudinary } from cloudinary;
+import cloudinaryModule from 'cloudinary';
+import dotenv from "dotenv";
+dotenv.config();
+
+const cloudinary = cloudinaryModule.v2;
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
 });
+
+
+
 export default cloudinary;
