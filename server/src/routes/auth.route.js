c165d427c675d91aa2280767c90facb514c65c36
nirajandhungel <subashdhungel555@gmail.com>
2025-08-27 14:40:15 +0545

    fix(route): fix update profile route using upload.single middleware

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { signup ,login, logout, updateProfile, checkAuth} from "../controllers/auth.controller.js";
+import upload from "../middleware/multer.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+  checkAuth,
+} from "../controllers/auth.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
-const  router = express.Router();
-router.post("/signup",signup);
-router.post("/login",login);
-router.post("/logout",logout);
-router.put("/update-profile",protect, updateProfile);
+const router = express.Router();
 
-router .get("/check-auth",protect,checkAuth);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.put(
+  "/update-profile",
+  protect,
+  upload.single("profileImage"),
+  updateProfile
+);
+
+router.get("/check-auth", protect, checkAuth);
 
 export default router;
