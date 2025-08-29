be862725c264932f04a6a3497a4bf70a848f5a01
nirajandhungel <subashdhungel555@gmail.com>
2025-08-29 21:54:16 +0545

    feat(home): update home with required feat

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar';
+import Welcome from '../components/Welcome';
+import ChatBox from '../components/ChatBox';
+import { useChatStore } from '../store/useChatStore.js';
 
 const Home = () => {
+  const {selecteduser} = useChatStore();
   return (
-    <div>
-      Home
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)] ">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar/>
+            {!selecteduser ? <Welcome/>:<ChatBox/>}
+  
+            </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
