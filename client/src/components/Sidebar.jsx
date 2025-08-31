3d943cc9245ef3e2badb292f8d57c051f5d29c36
nirajandhungel <subashdhungel555@gmail.com>
2025-08-31 23:15:59 +0545

    fix(onlineUsers): get online users from auth store

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react'
 import { Users } from "lucide-react";
 import { useChatStore } from '../store/useChatStore';
+import { useAuthStore } from '../store/useAuthStore.js';
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
 
 const Sidebar = () => {
+    const {onlineUsers} = useAuthStore();
     const {getUsers, users, selectedUser, setSelectedUser, isUsersLoading} = useChatStore();
-    const onlineUsers = [];
     useEffect(()=>{
         getUsers();
     },[getUsers]);
     if(isUsersLoading) return <SidebarSkeleton/>;
 
-    console.log("Users in Sidebar ",users);
   return (
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
         <div className="border-b border-base-300 w-full p-5">
@@ -23,7 +23,9 @@ const Sidebar = () => {
         </div>
         <div className="overflow-y-auto w-full py-3">
             {users.map((user)=>(
-                <button key={user._id} onClick={()=>setSelectedUser(user)} className={`w-full p-3 flex items-center gap-3 hover:bg-base-300 transition-colors ${selectedUser?._id === user._id ? 'bg-base-300 ring-1 ring-base-300':''}`}>
+                <button key={user._id} onClick={()=>{setSelectedUser(user);
+                    console.log("Selected User ",user);}
+                } className={`w-full p-3 flex items-center gap-3 hover:bg-base-300 transition-colors ${selectedUser?._id === user._id ? 'bg-base-300 ring-1 ring-base-300':''}`}>
                     <div className="relative mx-auto lg:mx-0">
                         <img src={user.profileImageURL ||"/avatar.png"} alt={user.fullName} className="w-12 h-12 rounded-full object-cover"/>
                         {onlineUsers.includes(user._id) && (<span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 ring-2 ring-zinc-900 rounded-full"></span>
