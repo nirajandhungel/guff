c90ed247eb962fa60e5aaf0c2ec5437213aa1934
nirajandhungel <subashdhungel555@gmail.com>
2025-08-31 23:17:11 +0545

    fix(typo): selecteduser => selectedUser

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,14 +5,14 @@ import ChatBox from '../components/ChatBox';
 import { useChatStore } from '../store/useChatStore.js';
 
 const Home = () => {
-  const {selecteduser} = useChatStore();
+  const {selectedUser} = useChatStore();
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)] ">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar/>
-            {!selecteduser ? <Welcome/>:<ChatBox/>}
+            {selectedUser ? <ChatBox/>:<Welcome/>}
   
             </div>
 
