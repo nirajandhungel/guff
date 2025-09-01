4b2b361ab6e48748c29a1376c0b3591e871416c0
nirajandhungel <subashdhungel555@gmail.com>
2025-09-01 22:40:23 +0545

    feat(send message): add send message action

--- client/src/store/useChatStore.js
+++ client/src/store/useChatStore.js
@@ -1,44 +1,58 @@
-import {create} from 'zustand'
-import toast from 'react-hot-toast';
-import { axiosInstance } from '../lib/axiosInstance';
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axiosInstance";
 import { API_PATHS } from "../lib/apiPaths";
 
-export const useChatStore = create((set) => ({
-    chats: [],
-    users:[],
-    selectedUser:null,
-    isUsersLoading:false,
-    isMessagesLoading:false,
+export const useChatStore = create((set, get) => ({
+  chats: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
 
-    getUsers: async () => {
-        set({isUsersLoading:true});
-        try {
-            const res = await axiosInstance.get(API_PATHS.MESSAGES.GET_USERS);
-            set({users:res.data.users});
-            console.log("Fetched users in chat store",res.data);
-        } catch (error) {
-            console.log(error);
-            toast.error("Failed to load the users");
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get(API_PATHS.MESSAGES.GET_USERS);
+      set({ users: res.data.users });
+      console.log("Fetched users in chat store", res.data);
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to load the users");
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
 
-        }finally{
-            set({isUsersLoading:false});
-        }
-    },
+  getChats: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(
+        API_PATHS.MESSAGES.GET_MESSAGES(userId)
+      );
+      set({ chats: res.data.messages });
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to load the chats");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
 
-    getChats: async (userId) => {
-        set({isMessagesLoading:true});
-        try {
-            const res = await axiosInstance.get(API_PATHS.MESSAGES.GET_MESSAGES(userId));
-            set({chats:res.data.messages});
-        } catch (error) {
-            console.log(error);
-            toast.error("Failed to load the chats");
-        }finally{
-            set({isMessagesLoading:false});
-        }
-    },
-    // todo: optimize this later
-    setSelectedUser: (selectedUser) => set({selectedUser:selectedUser, chats:[]})
-
-    
-}))
+  sendMessage: async (messageData) => {
+    const { selectedUser, chats } = get();
+    try {
+      const res = await axiosInstance.post(
+        API_PATHS.MESSAGES.SEND_MESSAGE(selectedUser._id),
+        messageData
+      );
+      set({ chats: [...chats, res.data.message] });
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data?.message || "Failed to send the message");
+    }
+  },
+  // todo: optimize this later
+  setSelectedUser: (selectedUser) =>
+    set({ selectedUser: selectedUser, chats: [] }),
+}));
