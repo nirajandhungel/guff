16a3fdaea3d644bf35e32b026b5b28cf787dc2e1
nirajandhungel <subashdhungel555@gmail.com>
2025-09-01 22:39:10 +0545

    feat(skeleton message): use skeletom message if message is loading

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,21 +2,30 @@ import React from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 const ChatBox = () => {
   const { chats, getChats, isMessagesLoading, selectedUser } = useChatStore();
-  
+
   React.useEffect(() => {
     getChats(selectedUser._id);
   }, [selectedUser._id, getChats]);
 
-  if (isMessagesLoading) return <div>Loading...</div>;
-  return(
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader></ChatHeader>
+        <MessageSkeleton />
+        <MessageInput></MessageInput>
+      </div>
+    );
+  }
+  return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader></ChatHeader>
       <p className="">messages...</p>
       <MessageInput></MessageInput>
     </div>
-  )
+  );
 };
 
 export default ChatBox;
