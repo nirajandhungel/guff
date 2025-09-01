3faacf04014ba1ae82d41d8571b78341ff8e22f8
nirajandhungel <subashdhungel555@gmail.com>
2025-09-01 21:58:53 +0545

    feat(message skeleton): skeleton for messages

---
+++ client/src/components/skeletons/MessageSkeleton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MessageSkeleton = () => {
+  return (
+    <div className="flex-1 overflow-y-auto p-4 space-y-4">
+      {[...Array(6)].map((_, index) => (
+        <div
+          key={index}
+          className={`chat ${index % 2 === 0 ? "chat-start" : "chat-end"}`}
+        >
+          <div className="chat-image avatar">
+            <div className="size-10 rounded-full">
+              <div className="skeleton w-full h-full rounded-full"></div>
+            </div>
+          </div>
+          <div className="chat-header mb-1">
+            <div className="skeleton h-4 w-16"></div>
+          </div>
+          <div className="chat-bubble bg-transparent p-0">
+            <div
+              className={`skeleton h-16 ${
+                index % 2 === 0 ? "w-[180px]" : "w-[240px]"
+              }`}
+            ></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MessageSkeleton;
